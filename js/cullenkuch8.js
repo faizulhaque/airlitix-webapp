7698d5be95a3113b5d0e1598b39f36c248685488
Cullen <cullenkuch@gmail.com>
2022-04-28 17:33:26 -0400

    array from

--- js/cullenkuch8.js
+++ js/cullenkuch8.js
@@ -225,12 +225,13 @@ function handleResetBtn(){
 //Keypad Button Functionality
 for (const btn of keypadBtns) {
   btn.addEventListener('click', function() {
-  	 let panelScreen = btn.parentNode.parentNode.getElementsByClassName('panel-screen')[0]
-     let gridMenus = panelScreen.children
+  	 let panelScreen = btn.parentNode.parentNode.getElementsByClassName('panel-screen')[0];
+     let gridMenus = Array.from(panelScreen.children);
      let currentGridDisplayed = null;
      console.log(gridMenus)
      console.log(currentGridDisplayed)
      for (let i = 0; i < gridMenus.length; i++){
+       console.log(gridMenus[i])
        if (gridMenus[i].style.display == 'grid'){
           currentGridDisplayed = gridMenus[i];
           console.log(currentGridDisplayed)
